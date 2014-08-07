bdbcbb4ca875141ee666427ce2a05205d8546986
Daniel Louw <daniel@dline.co.za>
2014-08-07 23:34:25 +0200

    * completed the prephase * updated the makefile to make it more cool * enabled the shuffling of values in the random array creator

--- src/PrepPhase.hpp
+++ src/PrepPhase.hpp
@@ -5,35 +5,53 @@
 #include <math.h>
 #include <iostream>
 
+#include "util.hpp"
 #include "FilePrinter.hpp"
 
 void CreateFileAndPopulate(int x, int y, int p, int n)
 {
-	stringstream fileName;
-	fileName << "I-File-" << x << "-" << y;
-	FilePrinter outFile (fileName.str());
 
-	for (int i = 0; i < 10; i++)
-	{
-		cout << "p = " << p << "| n = " << n << endl;
-		outFile.WriteToFile("blah|\n");
-	}
-
-	outFile.CloseFile();
 }
 
 void PerformPrepPhase()
 {
-	for(int x = 0; x <11; x++)
+	for(int x = 1; x <=11; x++)
 	{
 
-		float p = x/10;
+		 // p is the proportion of duplicates within the array. 0.0 is no duplicates, 1.0 is 100% duplicates.
+		float p = (x-1)/10;
 
 		for (int y = 1; y < 6; y++)
 		{
+			// n is the size of the array of numbers to be generated.
+			// The array size increases exponentially according to the function n = (100/3)*3^y
 			int n = (100/3)*pow(3,y);
 
-			CreateFileAndPopulate(x, 1, p, n);
+			stringstream fileName;
+			fileName << "I-File-" << x << "-" << y;
+			FilePrinter outFile (fileName.str());
+
+			for (int i = 0; i < 10; i++)
+			{
+				
+				cout << "Generating array with parameters " << "p = " << p << " and " << "n = " << n << endl;
+				vector<int> randomArray = GenerateRandomArray(n,p);
+				//PrintArray(randomArray);
+
+				stringstream stringifiedArray;
+				for (int elem : randomArray)
+				{
+					stringifiedArray << elem << "|"; 
+				}
+				stringifiedArray << endl; 
+
+				outFile.WriteToFile(stringifiedArray.str());
+			}
+
+			outFile.CloseFile();
+
+
+
 		}
 
 	}
